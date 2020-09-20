4a492fcf8d04b9d16083752d144ddbd24a951edb
mantuok <mantuok@gmail.com>
2020-09-20 23:48:48 +0300

    Изменена логика раскраски шкал

---
+++ js/stat.js
@@ -0,0 +1,116 @@
+const CLOUD_WIDTH = 420;
+const CLOUD_HEIGHT = 270;
+const CLOUD_X = 100;
+const CLOUD_Y = 10;
+const GAP = 10;
+const BAR_WIDTH = 40;
+const BAR_HEIGHT = 150;
+const RED_COLOR = 'rgba(255, 0, 0, 1)';
+const font = {
+  SIZE: '16px',
+  FAMILY: 'PT Mono',
+  COLOR: '#000000'
+};
+const saturation = {
+  MIN: 0,
+  MAX: 100
+};
+
+const createCloud = function (ctx, xCoordinate, yCoordinate, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(xCoordinate, yCoordinate, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+const styleText = function (ctx) {
+  ctx.fillStyle = font.COLOR;
+  ctx.font = `${font.SIZE} ${font.FAMILY}`;
+  ctx.textBaseline = 'hanging';
+}
+
+const colorBar = function (ctx, name) {
+  let blueColor = getRandomBlue();
+  ctx.fillStyle = (name === 'Вы') ? RED_COLOR : blueColor;
+};
+
+const getRandomBlue = function () {
+  let randomSaturation = Math.floor(Math.random() * ( saturation.MAX - saturation.MIN ) ) + saturation.MIN;
+  let randomBlue = `hsl(240, ${randomSaturation}%, 50%)`;
+  return randomBlue;
+}
+
+window.renderStatistics = function (ctx, names, times) {
+
+  createCloud(
+    ctx,
+    CLOUD_X + GAP,
+    CLOUD_Y + GAP,
+    'rgba(0, 0, 0, 0.7)'
+  );
+
+  createCloud(
+    ctx,
+    CLOUD_X,
+    CLOUD_Y,
+    '#ffffff'
+  );
+
+  styleText(ctx);
+
+  ctx.fillText(
+    'Ура вы победили!',
+    CLOUD_X + ( GAP * 2 ),
+    CLOUD_Y + ( GAP * 2 )
+  );
+
+  ctx.fillText(
+    'Список результатов:',
+    CLOUD_X + ( GAP * 2 ),
+    CLOUD_Y + ( GAP * 4 )
+  );
+
+  for (i = 0; i < names.length; i++) {
+
+    const roundedTimes = Math.round(times[i]);
+
+    styleText(ctx);
+
+    ctx.fillText (
+      roundedTimes,
+      CLOUD_X + ( GAP * 5 ) + (( GAP * 5 ) + BAR_WIDTH ) * i,
+      CLOUD_Y + ( GAP * 7.5 )
+    );
+
+    // if ( names[i] === 'Вы' ) {
+    //   ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    // } else {
+    //   const saturation = Math.random() * 100;
+    //   ctx.fillStyle = `hsl(240, ${saturation}%, 50%)`;
+    // };
+
+    colorBar(ctx, names[i]);
+
+    ctx.fillRect(
+      CLOUD_X + ( GAP * 5 ) + (( GAP * 5 ) + BAR_WIDTH ) * i,
+      CLOUD_Y + ( GAP * 9 ),
+      BAR_WIDTH,
+      BAR_HEIGHT
+    );
+
+    styleText(ctx);
+
+    ctx.fillText (
+      names[i],
+      CLOUD_X + ( GAP * 5 ) + (( GAP * 5 ) + BAR_WIDTH ) * i,
+      CLOUD_Y + ( GAP * 9 ) + BAR_HEIGHT + ( GAP / 2 )
+    );
+
+
+  }
+
+
+
+
+
+
+
+};
