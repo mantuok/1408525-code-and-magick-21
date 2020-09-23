bd559813aaa37eef22b4401daa7c8c8ceffbabf8
mantuok <mantuok@gmail.com>
2020-09-24 01:22:13 +0300

    Добавлены функции получения случайных цветов плаща, глаз

--- js/setup.js
+++ js/setup.js
@@ -2,11 +2,21 @@
 
 const NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
 const SURNAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
-
-const WIZARDS = [];
-
-const getWizardName = () => NAMES[getRandomNumber(0, NAMES.length)] + ` ` + SURNAMES[getRandomNumber(0, SURNAMES.length)];
+const CLOAKS = [
+  `rgb(101, 137, 164)`,
+  `rgb(241, 43, 107)`,
+  `rgb(146, 100, 161)`,
+  `rgb(56, 159, 117)`,
+  `rgb(215, 210, 55)`,
+  `rgb(0, 0, 0)`
+];
+const EYES = [`black`, `red`, `blue`, `yellow`, `green`];
 
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+const getWizardName = () => NAMES[getRandomNumber(0, NAMES.length)] + ` ` + SURNAMES[getRandomNumber(0, SURNAMES.length)];
+const getWizardCloak = () => CLOAKS[getRandomNumber(0, CLOAKS.length)];
+const getWIzardEyes = () => EYES[getRandomNumber(0, EYES.length)];
 
-console.log(getWizardName());
+const createWizard = function () {
+
+};
