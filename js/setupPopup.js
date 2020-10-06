957dd5fdfc9b930afaad2ae3d3cad2ecf16488de
mantuok <mantuok@gmail.com>
2020-10-07 01:28:35 +0300

    Реализовано перетаскивание окна настройки и его  возвращение на изначальную позицию при повторном открытии

--- js/setupPopup.js
+++ js/setupPopup.js
@@ -8,12 +8,21 @@
     ESC: `Escape`,
     ENTER: `Enter`
   };
+  let InitialCoords = {
+    x: 0,
+    y: 0
+  };
+
   const openPopup = () => {
     userDialog.classList.remove(`hidden`);
+    InitialCoords.x = userDialog.offsetLeft;
+    InitialCoords.y = userDialog.offsetTop;
     document.addEventListener(`keydown`, window.utils.invokeIfKeyIs(Key.ESC, closePopup));
   };
   const closePopup = () => {
     userDialog.classList.add(`hidden`);
+    userDialog.style.left = InitialCoords.x + `px`;
+    userDialog.style.top = InitialCoords.y + `px`;
     document.removeEventListener(`keydown`, window.utils.invokeIfKeyIs(Key.ESC, closePopup));
   };
 
